cbac43bdb406712d155dba9249b1ef7b8f102cad
Animeshmitra08 <animeshmitra003@gmail.com>
2024-05-28 03:20:35 +0530

    razor pay

--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,7 @@
-const express = require('express');
+const express = require("express");
 const Razorpay = require('razorpay');
 const cors = require('cors');
+const crypto = require('crypto');
 require("dotenv").config();
 
 const app = express();
@@ -12,14 +13,14 @@ app.use(express.urlencoded({extended:false}));
 app.use(cors());
 
 app.post("/order", async (req, res)=>{
-    const razorPay = new Razorpay({
-        key_id : process.env.RAZORPAY_KEY_ID,
-        key_secret : process.env.RAZORPAY_KEY_SECRET
-    });
+    const razorpay = new Razorpay({
+        key_id: process.env.RAZORPAY_KEY_ID,
+        key_secret: process.env.RAZORPAY_SECRET
+    })
 
     try {
         const options = req.body;
-        const order = await razorPay.orders.create(options);
+        const order = await razorpay.orders.create(options);
         if (!order) {
             return res.status(500).send("Error");
         }
@@ -29,6 +30,21 @@ app.post("/order", async (req, res)=>{
         res.status(500).send("Error");
     }
 });
+
+app.post("/order/validate",async (req, res)=>{
+    const { razorpay_order_id, razorpay_payment_id, razorpay_signature } = req.body;
+    const sha = crypto.createHmac("sha256", process.env.RAZORPAY_SECRET);
+    sha.update(`${razorpay_order_id}|${razorpay_payment_id}`);
+    const digest = sha.digest("hex");
+    if (digest !== razorpay_signature) {
+        return res.status(400).json({msg:"Transaction not legit!" });
+    }
+    res.json({
+        msg:"success",
+        orderId : razorpay_order_id,
+        paymentId : razorpay_payment_id,
+    });
+})
 
 app.listen(PORT, ()=>{
     console.log("listening on port", PORT);
